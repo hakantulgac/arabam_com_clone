405eb1e208fe8d7d36c2149a223f3749963b3a38
hakantulgac <hakantulgac@gmail.com>
2024-11-14 22:35:49 +0300

    add data for repeating items & provider

---
+++ client/src/data/data-provider.js
@@ -0,0 +1,16 @@
+import data from "./data.json";
+
+export const {
+    headerDropItems, 
+    footerMenuItems, 
+    footerBrands,
+    footerLeftMenuItems,
+    footerContactLinks,
+    mainMenuItems,
+    highlightItems,
+    sideMenuItems,
+    vehicleTitles,
+    coopItems,
+    newsComponents
+} = data;
+
