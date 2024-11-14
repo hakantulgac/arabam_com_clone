bccf9b24db7dfb295c7c83d903387d87c60ff995
hakantulgac <hakantulgac@gmail.com>
2024-11-14 22:38:24 +0300

    add functions to display formatted data in page

---
+++ client/src/service/useful-functions.js
@@ -0,0 +1,24 @@
+export const createImageUrl = (vehicleName) => {
+    const imageName = vehicleName.split(" ").join("") + ".jpg";
+    const url = "/vehicleImage/" + imageName;
+    return url;
+}
+
+export const customizeDate = (stringDate) => {
+    const date = new Date(stringDate);
+
+    const formatter = new Intl.DateTimeFormat('tr', { month: 'long' });
+    const month = formatter.format(date);
+
+    const day = date.getDate() < 10 ? `0${date.getDate()}` : date.getDate();
+
+    const result = { year: date.getFullYear(), month, day };
+
+    return result;
+};
+ 
+export const formattedPrice = (price) => {
+    const stringPrice = price.toLocaleString();
+    const result = stringPrice.replace(",", ".");
+    return result.replace(",", ".");
+}
